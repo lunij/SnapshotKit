ee3468032dbd5ffa7b12c9ce83ea66326632c731
Marc Schultz <lunij@users.noreply.github.com>
2022-03-11 15:20:02 +0100

    Improve scaling logic of paths

--- Tests/SnapshotTestingTests/CGPath.swift
+++ Tests/SnapshotTestingTests/CGPath.swift
@@ -19,38 +19,44 @@ extension NSTextField {
 extension CGPath {
     /// Creates an approximation of a heart at a 45º angle with a circle above, using all available element types:
     static var heart: CGPath {
-        let scale: CGFloat = 30.0
+        let scale: CGFloat = 30
         let path = CGMutablePath()
 
-        path.move(to: CGPoint(x: 0.0 * scale, y: 0.0 * scale))
-        path.addLine(to: CGPoint(x: 0.0 * scale, y: 2.0 * scale))
+        path.move(to: .zero)
+        path.addLine(to: CGPoint(x: 0, y: 2).scaled(by: scale))
         path.addQuadCurve(
-            to: CGPoint(x: 1.0 * scale, y: 3.0 * scale),
-            control: CGPoint(x: 0.125 * scale, y: 2.875 * scale)
+            to: CGPoint(x: 1, y: 3).scaled(by: scale),
+            control: CGPoint(x: 0.125, y: 2.875).scaled(by: scale)
         )
         path.addQuadCurve(
-            to: CGPoint(x: 2.0 * scale, y: 2.0 * scale),
-            control: CGPoint(x: 1.875 * scale, y: 2.875 * scale)
+            to: CGPoint(x: 2, y: 2).scaled(by: scale),
+            control: CGPoint(x: 1.875, y: 2.875).scaled(by: scale)
         )
         path.addCurve(
-            to: CGPoint(x: 3.0 * scale, y: 1.0 * scale),
-            control1: CGPoint(x: 2.5 * scale, y: 2.0 * scale),
-            control2: CGPoint(x: 3.0 * scale, y: 1.5 * scale)
+            to: CGPoint(x: 3, y: 1).scaled(by: scale),
+            control1: CGPoint(x: 2.5, y: 2).scaled(by: scale),
+            control2: CGPoint(x: 3, y: 1.5).scaled(by: scale)
         )
         path.addCurve(
-            to: CGPoint(x: 2.0 * scale, y: 0.0 * scale),
-            control1: CGPoint(x: 3.0 * scale, y: 0.5 * scale),
-            control2: CGPoint(x: 2.5 * scale, y: 0.0 * scale)
+            to: CGPoint(x: 2, y: 0).scaled(by: scale),
+            control1: CGPoint(x: 3, y: 0.5).scaled(by: scale),
+            control2: CGPoint(x: 2.5, y: 0).scaled(by: scale)
         )
-        path.addLine(to: CGPoint(x: 0.0 * scale, y: 0.0 * scale))
+        path.addLine(to: .zero)
         path.closeSubpath()
 
         path.addEllipse(in: CGRect(
-            origin: CGPoint(x: 2.0 * scale, y: 2.0 * scale),
+            origin: CGPoint(x: 2, y: 2).scaled(by: scale),
             size: CGSize(width: scale, height: scale)
         ))
 
         return path
+    }
+}
+
+private extension CGPoint {
+    func scaled(by value: CGFloat) -> CGPoint {
+        .init(x: x * value, y: y * value)
     }
 }
 #endif
@@ -62,42 +68,40 @@ extension UIBezierPath {
         UIBezierPath(cgPath: .heart)
     }
 }
-#endif
-
-#if os(macOS)
+#elseif os(macOS)
 extension NSBezierPath {
     /// Creates an approximation of a heart at a 45º angle with a circle above, using all available element types:
     static var heart: NSBezierPath {
-        let scale: CGFloat = 30.0
+        let scale: CGFloat = 30
         let path = NSBezierPath()
 
-        path.move(to: CGPoint(x: 0.0 * scale, y: 0.0 * scale))
-        path.line(to: CGPoint(x: 0.0 * scale, y: 2.0 * scale))
+        path.move(to: .zero)
+        path.line(to: CGPoint(x: 0, y: 2).scaled(by: scale))
         path.curve(
-            to: CGPoint(x: 1.0 * scale, y: 3.0 * scale),
-            controlPoint1: CGPoint(x: 0.0 * scale, y: 2.5 * scale),
-            controlPoint2: CGPoint(x: 0.5 * scale, y: 3.0 * scale)
+            to: CGPoint(x: 1, y: 3).scaled(by: scale),
+            controlPoint1: CGPoint(x: 0, y: 2.5).scaled(by: scale),
+            controlPoint2: CGPoint(x: 0.5, y: 3).scaled(by: scale)
         )
         path.curve(
-            to: CGPoint(x: 2.0 * scale, y: 2.0 * scale),
-            controlPoint1: CGPoint(x: 1.5 * scale, y: 3.0 * scale),
-            controlPoint2: CGPoint(x: 2.0 * scale, y: 2.5 * scale)
+            to: CGPoint(x: 2, y: 2).scaled(by: scale),
+            controlPoint1: CGPoint(x: 1.5, y: 3).scaled(by: scale),
+            controlPoint2: CGPoint(x: 2, y: 2.5).scaled(by: scale)
         )
         path.curve(
-            to: CGPoint(x: 3.0 * scale, y: 1.0 * scale),
-            controlPoint1: CGPoint(x: 2.5 * scale, y: 2.0 * scale),
-            controlPoint2: CGPoint(x: 3.0 * scale, y: 1.5 * scale)
+            to: CGPoint(x: 3, y: 1).scaled(by: scale),
+            controlPoint1: CGPoint(x: 2.5, y: 2).scaled(by: scale),
+            controlPoint2: CGPoint(x: 3, y: 1.5).scaled(by: scale)
         )
         path.curve(
-            to: CGPoint(x: 2.0 * scale, y: 0.0 * scale),
-            controlPoint1: CGPoint(x: 3.0 * scale, y: 0.5 * scale),
-            controlPoint2: CGPoint(x: 2.5 * scale, y: 0.0 * scale)
+            to: CGPoint(x: 2, y: 0).scaled(by: scale),
+            controlPoint1: CGPoint(x: 3, y: 0.5).scaled(by: scale),
+            controlPoint2: CGPoint(x: 2.5, y: 0).scaled(by: scale)
         )
-        path.line(to: CGPoint(x: 0.0 * scale, y: 0.0 * scale))
+        path.line(to: CGPoint(x: 0, y: 0).scaled(by: scale))
         path.close()
 
         path.appendOval(in: CGRect(
-            origin: CGPoint(x: 2.0 * scale, y: 2.0 * scale),
+            origin: CGPoint(x: 2, y: 2).scaled(by: scale),
             size: CGSize(width: scale, height: scale)
         ))
 
